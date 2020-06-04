278ef69ed448e9d333fee6a08a6f9f902793b7ab
Sushant More <moresushant48@gmail.com>
2020-06-04 20:15:17 +0530

    Setup Themes and Volunteer Form page.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { VolunteerRegisterComponent } from './volunteer-register/volunteer-register.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/volunteer-register/volunteer-register.component.ts
+++ src/app/volunteer-register/volunteer-register.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+declare var $ : any;
 
 @Component({
   selector: 'app-volunteer-register',
   templateUrl: './volunteer-register.component.html',
   styleUrls: ['./volunteer-register.component.css']
 })
+
 export class VolunteerRegisterComponent implements OnInit {
 
-  constructor() { }
+  volunteerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.blankForm();
   }
 
+  blankForm(){
+    this.volunteerForm = this.fb.group({
+      vFirstName: '',
+      vLastName: '',
+      vEmailId: '',
+      vMobileNo: '',
+      vAddress: ''
+    })
+  }
+
+  registerVolunteer(volunteer : Volunteer) {
+    
+    $("#result").html("Processing...");
+
+    this.blankForm();
+    
+    this.http.post('http://localhost:9090/api/post/volunteer', volunteer).subscribe(
+      (response) => $("#result").html("Registered Succefully."),
+      (error) => console.log(error)
+    ) 
+  }
+}
+
+export class Volunteer {
+  vFirstName: string;
+  vLastName: string;
+  vEmailId: string;
+  vMobileNo: string;
+  vAddress: string;
 }
