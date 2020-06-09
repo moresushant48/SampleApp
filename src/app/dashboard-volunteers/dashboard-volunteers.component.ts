9181c23691a8e87135f0ad5d72e31898627cd563
Sushant More <moresushant48@gmail.com>
2020-06-09 15:30:48 +0530

    Dashboard : Setup DATATABLES.NET for Volunteer List.
    Dependencies and things :
    npm install --save jszip
    npm install --save pdfmake
    npm install --save datatables.net
    npm install --save datatables.net-bs4
    npm install --save datatables.net-buttons-bs4
    npm install --save datatables.net-responsive-bs4
    
    Css to be imported :
    "./node_modules/datatables.net-bs4/css/dataTables.bootstrap4.css"
    "./node_modules/datatables.net-buttons-bs4/css/buttons.bootstrap4.css"
    "./node_modules/datatables.net-responsive-bs4/css/responsive.bootstrap4.css"
    
    Scripts to be imported :
    "./node_modules/datatables.net/js/jquery.dataTables.js"
    "./node_modules/datatables.net-bs4/js/dataTables.bootstrap4.js"
    "./node_modules/datatables.net-buttons/js/dataTables.buttons.js"
    "./node_modules/datatables.net-buttons-bs4/js/buttons.bootstrap4.js"
    "./node_modules/jszip/dist/jszip.js"
    "./node_modules/pdfmake/build/pdfmake.js"
    "./node_modules/pdfmake/build/vfs_fonts.js"
    "./node_modules/datatables.net-buttons/js/buttons.colVis.js"
    "./node_modules/datatables.net-buttons/js/buttons.flash.js"
    "./node_modules/datatables.net-buttons/js/buttons.html5.js"
    "./node_modules/datatables.net-buttons/js/buttons.print.js"
    "./node_modules/datatables.net-responsive/js/dataTables.responsive.js"
    "./node_modules/datatables.net-responsive-bs4/js/responsive.bootstrap4.js"
    
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/app/dashboard-volunteers/dashboard-volunteers.component.ts
+++ src/app/dashboard-volunteers/dashboard-volunteers.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-dashboard-volunteers',
   templateUrl: './dashboard-volunteers.component.html',
@@ -10,6 +12,23 @@ export class DashboardVolunteersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    $('#tblVol').DataTable({
+      dom: 'Bfrltip',
+      "sAjaxSource": "http://localhost:9090/api/get/volunteers",
+      "sAjaxDataProp": "",
+      "order": [0, "desc"],
+      "columnDefs": [{ targets: 5, sortable: false }],
+      "aoColumns": [
+        { "mData": "vId" },
+        { "mData": "vFirstName" },
+        { "mData": "vLastName" },
+        { "mData": "vMobileNo" },
+        { "mData": "vEmailId" },
+        { "mData": "vAddress" },
+      ]
+    });
+
   }
 
 }
